8ba7444fea246dc4a0e363dd8ba3b05090b5fd87
Aleksey Belchenkov <u608110@gmail.com>
2019-07-09 06:24:34 +0300

    Header & Footer

---
+++ src/components/Header-Footer/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer>
+        <div class="container">
+            <div class="logo">
+                GameSpot
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
