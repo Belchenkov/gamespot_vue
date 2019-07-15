2cd450bfc26492fa13007fe9d5b43444e2279b81
Aleksey Belchenkov <u608110@gmail.com>
2019-07-15 07:13:30 +0300

    Adding home content

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <comp-featured />
+        <comp-content />
     </div>
 </template>
 
 <script>
     import compFeatured from './featured';
+    import compContent from './content';
 
     export default {
         name: "index",
         components: {
-            compFeatured
+            compFeatured,
+            compContent
         }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './routes';
 
+import { MdCard } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+
+Vue.use(MdCard);
+
 import Button from './components/UI/button';
 Vue.component('app-button', Button);
 
