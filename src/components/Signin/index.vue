ab48adabfac3e1316645f4e1dc8f3459d9f47c8b
Aleksey Belchenkov <u608110@gmail.com>
2019-07-16 07:06:49 +0300

    Login Form

---
+++ src/components/Signin/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="signin_container">
+            <h1>Sign in</h1>
+
+            <form @submit.prevent="onSubmit">
+                <div class="input_field">
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        v-model="formData.email"
+                    >
+                </div>
+                <div class="input_field">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        v-model="formData.password"
+                    >
+                </div>
+                <button type="submit">Sign in</button>
+
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data() {
+            return {
+                formData: {
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            onSubmit() {
+               console.log(this.formData);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueResource from 'vue-resource';
 import router from './routes';
 
 import { MdCard } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 
+// Material
 Vue.use(MdCard);
+
+// Resource
+Vue.use(VueResource);
+Vue.http.options.root = '';
 
 import Button from './components/UI/button';
 Vue.component('app-button', Button);
