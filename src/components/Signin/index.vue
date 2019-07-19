4bb6411914e2d541c23b8cadd35c3a2bf7e4a03a
Aleksey Belchenkov <u608110@gmail.com>
2019-07-19 07:11:13 +0300

    Login Validation

--- src/components/Signin/index.vue
+++ src/components/Signin/index.vue
@@ -9,40 +9,102 @@
                     <input
                         type="email"
                         v-model="formData.email"
+                        @blur="$v.formData.email.$touch()"
+                        :class="{invalid: $v.formData.email.$error}"
                     >
+                    <div v-if="$v.formData.email.$error">
+                        <p
+                            class="error_label"
+                            v-if="!$v.formData.email.required"
+                        >
+                            This field is required
+                        </p>
+                        <p
+                            class="error_label"
+                            v-if="!$v.formData.email.email"
+                        >
+                            Please enter a valid email
+                        </p>
+                    </div>
                 </div>
-                <div class="input_field">
+                <div
+                    class="input_field"
+                    :class="{invalid: $v.formData.email.$error}"
+                >
                     <label>Password</label>
                     <input
                         type="password"
                         v-model="formData.password"
+                        @blur="$v.formData.password.$touch()"
                     >
                 </div>
+                <div v-if="$v.formData.password.$error">
+                    <p
+                            class="error_label"
+                            v-if="!$v.formData.password.required"
+                    >
+                        This field is required
+                    </p>
+                    <p
+                            class="error_label"
+                            v-if="!$v.formData.password.minLength"
+                    >
+                        At least 4 characters
+                    </p>
+                </div>
                 <button type="submit" style="cursor: pointer">Sign in</button>
+                <p class="error_label" v-if="error">
+                    Something is wrong
+                </p>
             </form>
         </div>
     </div>
 </template>
 
 <script>
+    import { required, email, minLength } from 'vuelidate/lib/validators';
+
     export default {
         name: "index",
         data() {
             return {
+                error: false,
                 formData: {
-                    email: 'u608110@gmail.com',
-                    password: '12qwasZX'
+                    email: '',
+                    password: ''
                 }
+            }
+        },
+        validations: {
+            formData: {
+                email: {
+                    required,
+                    email
+                },
+                password: {
+                    required,
+                    minLength: minLength(4)
+                },
             }
         },
         methods: {
             onSubmit() {
-               this.$store.dispatch('admin/signIn', this.formData);
+                if (!this.$v.$invalid) {
+                    this.$store.dispatch('admin/signIn', this.formData);
+                } else {
+                    this.error = true;
+                    setTimeout(() => {
+                        this.error = false;
+                    }, 3000);
+                }
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .input_field.invalid input,
+    .input_field.invalid select {
+        border: 1px solid red;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
-import router from './routes';
+import vuelidate from 'vuelidate';
 
 import store from './Store/store';
+import router from './routes';
 
 import { MdCard } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 
 // Material
 Vue.use(MdCard);
+import Button from './components/UI/button';
+Vue.component('app-button', Button);
+
+// Misc
+Vue.use(vuelidate);
 
 // Resource
 Vue.use(VueResource);
 Vue.http.options.root = '';
 
-import Button from './components/UI/button';
-Vue.component('app-button', Button);
 
 new Vue({
   router,
